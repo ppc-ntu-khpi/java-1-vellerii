584a33f5b7a395ff22da2b0bd1336433cec5765e
Valeriia Krychevska <90756841+vellerii@users.noreply.github.com>
2023-01-29 00:34:57 +0200

    Update CustomerTest.java

--- src/test/CustomerTest.java
+++ src/test/CustomerTest.java
@@ -5,6 +5,13 @@ public class CustomerTest {
     public static void main(String args[]) {
         Customer theCustomer = new Customer();
 
-        theCustomer.displayCustomerInfo();
+        theCustomer.setID(456);
+        System.out.println("Customer ID: " + theCustomer.getID());
+        
+        theCustomer.setStatus(true);
+        System.out.println("Is the customer new: " + theCustomer.getStatus());
+        
+        theCustomer.setTotal(7905.5f);
+        System.out.println("Total purchases are: "  + theCustomer.getTotal());
     }
 }
