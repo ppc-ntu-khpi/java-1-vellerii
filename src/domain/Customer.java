6fb807ff3c73f682fe57bc01aa2df1627b876ad6
Valeriia Krychevska <90756841+vellerii@users.noreply.github.com>
2023-01-29 00:33:39 +0200

    Update Customer.java

--- src/domain/Customer.java
+++ src/domain/Customer.java
@@ -1,9 +1,43 @@
 package domain;
 
 public class Customer {
-    private int ID = 16061;
-    private boolean isNew = true;
-    private float total = 720.4f;
+    private int ID;
+    private boolean isNew;
+    private float total;
+    
+    public Customer() {
+       ID = 16061;
+       isNew = true;
+       total = 720.4F;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        if (ID >= 0) {
+             this.ID = ID;
+        }
+    }
+
+    public boolean getStatus() {
+        return isNew;
+    }
+
+    public void setStatus(boolean isNew) {
+        this.isNew = isNew;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        if (total >= 0) {
+           this.total = total;
+        }
+    }
 
     public void displayCustomerInfo() {
         System.out.println("Customer ID: " + ID);
